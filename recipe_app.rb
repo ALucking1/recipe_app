cf0b756fa5de02842de1414b52966330410c83b4
Anna Lucking <annavlucking@hotmail.co.uk>
2016-12-26 17:37:25 +0000

    add functioning openweather api call for current temp in London

--- recipe_app.rb
+++ recipe_app.rb
@@ -4,10 +4,13 @@ require 'rest-client'
 
 class WeatherRecipe < Sinatra::Base
   get '/' do
-    erb :index
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4d9ad4b01c56f6638dcdd451d752a2f'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=b4d9ad4b01c56f6638dcdd451d752a2f'
     response = RestClient.get(url)
-    JSON.parse(response)
+    hash = JSON.parse(response)
+    hash['main'].each do |x|
+      @value = x[1]
+    end
+    erb :index
   end
 
   # start the server if ruby file executed directly
