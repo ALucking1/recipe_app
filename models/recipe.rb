5c183dfecf583cf91a0e82fca8d0b0cf3afea751
Anna Lucking <annavlucking@hotmail.co.uk>
2016-12-26 17:06:23 +0000

    add openweather api - incomplete

---
+++ models/recipe.rb
@@ -0,0 +1,6 @@
+# require 'json'
+
+# api_result = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4d9ad4b01c56f6638dcdd451d752a2f"
+
+
+# {"_id":2643743,"name":"London","country":"GB","coord":{"lon":-0.12574,"lat":51.50853}}
--- recipe_app.rb
+++ recipe_app.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
+require 'json'
+require 'rest-client'
 
 class WeatherRecipe < Sinatra::Base
   get '/' do
     erb :index
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4d9ad4b01c56f6638dcdd451d752a2f'
+    response = RestClient.get(url)
+    JSON.parse(response)
   end
 
   # start the server if ruby file executed directly
