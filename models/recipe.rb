0c08dba5fd73d82cbb4c34c334ca9efffd0e00e2
Anna Lucking <annavlucking@hotmail.co.uk>
2016-12-27 21:20:38 +0000

    some logic - not implemented

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,6 +1,17 @@
-# require 'json'
+class Recipe
 
-# api_result = "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b4d9ad4b01c56f6638dcdd451d752a2f"
+  def intro
+    if @value < 10
+       'Brrr!'
+    elsif @value <15 && @value >10
+      'Hmm, not too cold.'
+    elsif @value < 20 && @value > 15
+      'Mild, mild, mild.'
+    elsif @value < 30 && @value > 20
+      'Well, it\'s positively balmy!'
+    else
+      'You want to eat?!'
+    end
+  end
 
-
-# {"_id":2643743,"name":"London","country":"GB","coord":{"lon":-0.12574,"lat":51.50853}}
+end
