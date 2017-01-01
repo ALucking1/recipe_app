0e84ffe641cccbd09ad28c7a5b43a1efae27cb48
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-01 12:37:45 +0000

    refactored api out of controller - not selecting right part of hash

--- recipe_app.rb
+++ recipe_app.rb
@@ -1,15 +1,11 @@
 require 'sinatra/base'
 require 'json'
 require 'rest-client'
+require './models/weather.rb'
 
 class WeatherRecipe < Sinatra::Base
   get '/' do
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=b4d9ad4b01c56f6638dcdd451d752a2f'
-    response = RestClient.get(url)
-    hash = JSON.parse(response)
-    hash['main'].each do |x|
-      @value = x[1]
-    end
+    @weather = WeatherApi.new
     erb :index
   end
 
