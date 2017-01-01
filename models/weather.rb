bce5d03836be1093c25c04ad8d7fdab0aee9838c
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-01 12:52:05 +0000

    refactor get_api method

--- models/weather.rb
+++ models/weather.rb
@@ -1,13 +1,11 @@
 class WeatherApi
 
-  attr_reader :value
-
   def get_api
     url = 'http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=b4d9ad4b01c56f6638dcdd451d752a2f'
     response = RestClient.get(url)
     hash = JSON.parse(response)
     hash['main'].each do |x|
-      return @value = x[1]
+      return x[1]
     end
   end
 
