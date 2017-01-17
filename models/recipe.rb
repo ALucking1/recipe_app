3ae9170920e25e52718302fbebd9fbab5e7decff
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-17 17:16:28 +0000

    link to recipe - title not working yet

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,6 +1,21 @@
 class Recipe
 
-  def cold_days
-     ["http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", "https://deliciouslyella.com/potato-peanut-curry/", "http://www.bbcgoodfood.com/recipes/4807/chunky-minestrone-soup"].sample
+
+
+  def brrr_days
+     [{:url => "http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", :title => "Chilli Con Carne"}, {:url => "https://deliciouslyella.com/potato-peanut-curry/", :title => "Potato and Peanut Curry"}, {:url => "http://www.bbcgoodfood.com/recipes/4807/chunky-minestrone-soup", :title => "Minestrone Soup"}].sample
   end
+
+  def not_too_cold_days
+  end
+
+  def mild_days
+  end
+
+  def balmy_days
+  end
+
+  def hot_days
+  end
+
 end
