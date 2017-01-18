dd8b1e0e32cf83b04e3945fc54b07c3eb8cbe1e4
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-18 15:46:33 +0000

    recipes and links on randomise working

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,23 +1,25 @@
 class Recipe
 
+  attr_reader :rand_rec
+
   def brrr_days
-    {:url => "http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", :title => "Chilli Con Carne"}
+    @rand_rec = [["http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", "Chilli Con Carne"], ["https://deliciouslyella.com/potato-peanut-curry/", "Potato Peanut Curry"], ["http://www.bbcgoodfood.com/recipes/4807/chunky-minestrone-soup", "Minestrone Soup"]].sample
   end
 
   def not_too_cold_days
-    {:url => "http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", :title => "Tuna Sweet Potato Jackets"}
+    @rand_rec = [["http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", "Tuna Sweet Potato Jackets"], ["http://www.bbc.co.uk/food/recipes/sausage_rolls_with_86029", "Sausage Rolls with Caramelised Onions"], ["http://www.bbc.co.uk/food/recipes/pork_belly_with_apples_71757", "Pork Belly with Apple and Sage"]].sample
   end
 
   def mild_days
-    {:url => "http://www.bbc.co.uk/food/recipes/pork_pie_with_chicken_02918", :title => "Pork Pie with Chicken and Apricots"}
+    @rand_rec = [["http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", "Tuna Sweet Potato Jackets"], ["http://www.bbc.co.uk/food/recipes/carrot_and_poppyseed_72665", "Carrot and Poppyseed Salad"], ["http://www.bbc.co.uk/food/recipes/pork_pie_with_chicken_02918", "Pork Pie with Chicken and Apricots"]].sample
   end
 
   def balmy_days
-    {:url => "http://www.bbc.co.uk/food/recipes/quichelorraine_71987", :title => "Quiche Lorraine"}
+    @rand_rec = [["http://www.bbc.co.uk/food/recipes/quichelorraine_71987", "Quiche Lorraine"], ["http://www.bbc.co.uk/food/recipes/scottish_cranachan_with_74862", "Scottish Cranachan"], ["http://www.bbc.co.uk/food/recipes/mixed_bean_salad_89055", "Mixed Bean Salad"]].sample
   end
 
   def hot_days
-    {:url => "http://www.bbc.co.uk/food/recipes/strawberrydiaquiri_86315", :title => "Strawberry Daiquiri"}
+    @rand_rec = [["http://www.bbc.co.uk/food/recipes/strawberrydiaquiri_86315", "Strawberry Daiquiri"], ["http://www.bbc.co.uk/food/recipes/coconuticecream_86100", "Coconut Icecream"], ["http://www.bbc.co.uk/food/recipes/pistachio_and_rose_ice_66443", "Pistachio and Rose Icecream"]].sample
   end
 
 end
