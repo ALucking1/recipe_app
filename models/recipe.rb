c0ad2ef42204c6c5ded206f939ee8ae0a4cae7d8
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-18 13:59:48 +0000

    recipes working - when only one to be chosen

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,23 +1,23 @@
-# class Recipe
-#
-#   attr_reader :brrr_days
-#
-#   @brrr_days = {:url => "http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", :title => "Chilli Con Carne"}
+class Recipe
 
-  # def not_too_cold_days
-  #   [{:url => "http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", :title => "Tuna Sweet Potato Jackets"}, {:url => "http://www.bbc.co.uk/food/recipes/sausage_rolls_with_86029", :title => "Sausage Rolls with Caramelised Onions"}, {:url => "http://www.bbc.co.uk/food/recipes/pork_belly_with_apples_71757", :title => "Pork Belly with Apple and Sage"}].sample
-  # end
-  #
-  # def mild_days
-  #   [{:url => "http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", :title => "Tuna Sweet Potato Jackets"}, {:url => "http://www.bbc.co.uk/food/recipes/carrot_and_poppyseed_72665", :title => "Carrot and Poppyseed Salad"}, {:url => "http://www.bbc.co.uk/food/recipes/pork_pie_with_chicken_02918", :title => "Pork Pie with Chicken and Apricots"}].sample
-  # end
-  #
-  # def balmy_days
-  #   [{:url => "http://www.bbc.co.uk/food/recipes/quichelorraine_71987", :title => "Quiche Lorraine"}, {:url => "http://www.bbc.co.uk/food/recipes/scottish_cranachan_with_74862", :title => "Scottish Cranachan"}, {:url => "http://www.bbc.co.uk/food/recipes/mixed_bean_salad_89055", :title => "Mixed Bean Salad"}].sample
-  # end
-  #
-  # def hot_days
-  #   [{:url => "http://www.bbc.co.uk/food/recipes/strawberrydiaquiri_86315", :title => "Strawberry Daiquiri"}, {:url => "http://www.bbc.co.uk/food/recipes/coconuticecream_86100", :title => "Coconut Icecream"}, {:url => "http://www.bbc.co.uk/food/recipes/pistachio_and_rose_ice_66443", :title => "Pistachio and Rose Icecream"}].sample
-  # end
+  def brrr_days
+    {:url => "http://www.bbcgoodfood.com/recipes/3444/saras-chilli-con-carne", :title => "Chilli Con Carne"}
+  end
 
-# end
+  def not_too_cold_days
+    {:url => "http://www.bbcgoodfood.com/recipes/144605/tuna-sweet-potato-jackets", :title => "Tuna Sweet Potato Jackets"}
+  end
+
+  def mild_days
+    {:url => "http://www.bbc.co.uk/food/recipes/pork_pie_with_chicken_02918", :title => "Pork Pie with Chicken and Apricots"}
+  end
+
+  def balmy_days
+    {:url => "http://www.bbc.co.uk/food/recipes/quichelorraine_71987", :title => "Quiche Lorraine"}
+  end
+
+  def hot_days
+    {:url => "http://www.bbc.co.uk/food/recipes/strawberrydiaquiri_86315", :title => "Strawberry Daiquiri"}
+  end
+
+end
